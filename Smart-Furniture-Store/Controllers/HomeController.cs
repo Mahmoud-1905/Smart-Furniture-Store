b347d54ff33c9583f3810e5079c1f0d27bf4ccfa
Raghad Akila <raghadakila@gmail.com>
2026-03-30 12:55:05 +0300

    Add ASP.NET Core MVC project structure (Controllers, Models, Views)

---
+++ Smart-Furniture-Store/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Smart_Furniture_Store.Models;
+
+namespace Smart_Furniture_Store.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Smart-Furniture-Store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Smart_Furniture_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
